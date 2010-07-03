1fbe1916fe345be21fdf7cde1f49e9d523400f32
Johannes Emerich <johannes@emerich.de>
2010-07-03 15:52:04 +0200

    Add logger (and ignore logfiles)

--- tools/logger.py
+++ tools/logger.py
@@ -1,4 +1,18 @@
+import os
+from datetime import datetime
+
 def log(message):
-	pass
+	try:
+		with open(_pathToLogs(), 'a') as f:
+			f.write('%s (%s)' % (message, datetime.now().ctime()))
+	except IOError:
+		pass
+
+def _pathToLogs(logType = 'error'):
+	""" Returns pathname for logfile which is being created if not yet present. """
+	logsPath = os.path.join(os.path.dirname(__file__), '../logs')
+	if not os.path.isdir(logsPath):
+		os.mkdir(logsPath)
+	return os.path.join(logsPath, '%s.log' % (logType))
 
 # See PEP 366 for package issue
