d60723492c4cbbf07a4fd4f56d01f11d36641db6
Johannes Emerich <johannes@emerich.de>
2010-07-03 16:05:30 +0200

    Add logger.log/.debug/.error

--- tools/logger.py
+++ tools/logger.py
@@ -1,14 +1,23 @@
 import os
 from datetime import datetime
 
+def debug(message):
+	_recordLine(message, 'debug')
+
+def error(message):
+	_recordLine(message, 'error')
+
 def log(message):
+	_recordLine(message, 'journal')
+
+def _recordLine(message, logType):
 	try:
-		with open(_pathToLogs(), 'a') as f:
-			f.write('%s (%s)' % (message, datetime.now().ctime()))
+		with open(_pathToLogs(logType), 'a') as f:
+			f.write('%s (%s)\n' % (message, datetime.now().ctime()))
 	except IOError:
 		pass
 
-def _pathToLogs(logType = 'error'):
+def _pathToLogs(logType):
 	""" Returns pathname for logfile which is being created if not yet present. """
 	logsPath = os.path.join(os.path.dirname(__file__), '../logs')
 	if not os.path.isdir(logsPath):
