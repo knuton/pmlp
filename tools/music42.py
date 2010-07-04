a7792db4998312751cd8d98307e280fb37005bfb
Johannes Emerich <johannes@emerich.de>
2010-07-04 13:14:15 +0200

    Add denormalizing function

--- tools/music42.py
+++ tools/music42.py
@@ -1,19 +1,35 @@
 import music21
 import logger
 
+cFour = music21.note.Note()
+
 def normalizeNotes(noteSequence = []):
 	# We need to use an actual note with pitch to determine the distance to C4,
 	# not a rest or other note-like object.
-	firstNote = next((note for note in noteSequence if note.isNote), None)
+	firstNote = firstActualNote(noteSequence)
 	# notesToInterval uses C4 by default
 	if firstNote:
 		distanceToC = music21.interval.notesToInterval(firstNote)
 		noteSequence = music21.stream.Stream(noteSequence).transpose(distanceToC)
 	return tuple([makeNote(note) for note in noteSequence])
-	
+
+def firstActualNote(noteSequence):
+	return next((note for note in noteSequence if note.isNote), None)
+
+def denormalizeNote(note, unnormalHistory):
+	""" De-normalizes a note generated form a normalized history using its unnormalized history. """
+	if note.isNote:
+		by = firstActualNote(unnormalHistory)
+		logger.debug(str(by) + ' applied to ' + str(note)) # DEBUG
+		return by and note.transpose(music21.interval.notesToInterval(cFour, by)) or note
+	else:
+		return note
+
 def makeNote(noteObj):
 	if noteObj.isNote:
-		return music21.note.Note(str(noteObj.pitch), type = noteObj.duration.quarterLength)
+		note = music21.note.Note(type = noteObj.duration.quarterLength)
+		note.pitch.midi = noteObj.pitch.midi
+		return note
 	elif noteObj.isRest:
 		rest = music21.note.Rest()
 		rest.duration = music21.duration.Duration(noteObj.duration.quarterLength)
