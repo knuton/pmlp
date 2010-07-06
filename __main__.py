47feade94132cdcf4cebb11bf95e8c72961bc58e
Johannes Emerich <johannes@emerich.de>
2010-07-06 15:11:10 +0200

    Add UI for pmlp

---
+++ __main__.py
@@ -0,0 +1,101 @@
+import music21.corpus
+import corpus
+import training
+from tools import logger
+
+logo = """
+                 .__          
+  ______   _____ |  | ______  
+  \____ \ /     \|  | \____ \ 
+  |  |_> >  Y Y  \  |_|  |_> >
+  |   __/|__|_|  /____/   __/ 
+  |__|         \/     |__|    """
+
+def startup():
+	print logo
+	menu()
+
+def menu():
+	choice = 0
+	while not choice in ['l', 'g', 'h', 'q']:
+		print "\n  $ MENU\n"
+		print "  (l) List corpora"
+		print "  (g) Generate a song"
+		print "  (h) Help"
+		print "  (q) Quit\n"
+		try:
+			choice = str(raw_input("  Make your choice by entering one of l, g, h and q > "))
+		except ValueError:
+			print "  Please enter an item's number."
+	
+	if choice == 'q':
+		exit(0)
+	elif choice == 'h':
+		print "  We will add a help text later. Purhaps."
+		menu()
+	elif choice == 'g':
+		collectionName = None
+		corpusName = None
+		xmls = []
+		while not collectionName in ['pmlp', 'music21']:
+			collectionName = str(raw_input("  Generate from PMLP (`pmlp`) or music21 (`music21`) corpus? (Abort with `a`) > "))
+			if collectionName == 'pmlp':
+				corpusName = chooseCorpus('pmlp')
+				xmls = corpus.getCollection(corpusName)
+			elif collectionName == 'music21':
+				corpusName = chooseCorpus('music21')
+				xmls = music21.corpus.getComposer(corpusName)
+			elif collectionName == 'a':
+				break
+		if corpusName:
+			generate(collectionName, corpusName, xmls)
+	elif choice == 'l':
+		listCorpora()
+		raw_input("\n  Enter to return to menu.")
+		menu()
+
+def listCorpora(selected = None):
+	if not selected:
+		print '\n  $ CORPORA'
+	
+	if not selected or selected == 'pmlp':
+		print "\n  $$ PMLP\n"
+		pmlpCorpora = corpus.listCollections()
+		for i in range(0, len(pmlpCorpora)):
+			print "  " + str(i + 1) + ". " + str(pmlpCorpora[i]), not selected and "(PMLP)" or ''
+		if selected:
+			return pmlpCorpora
+	if not selected or selected == 'music21':
+		print "\n  $$ music21\n"
+		music21Corpora = [composer[0] for composer in music21.corpus.COMPOSERS]
+		for i in range(0, len(music21Corpora)):
+			print "  " + str(i + 1) + ". " + str(music21Corpora[i]),  not selected and "(music21)" or ''
+		if selected:
+			return music21Corpora
+	
+	return []
+
+def chooseCorpus(selected):
+	if not selected:
+		raise ValueError('Need to select a corpus collection.')
+	
+	choice = None
+	possibilities = listCorpora(selected)
+	
+	while not choice in possibilities:
+		if not choice == None:
+			print "\n  Please enter the name of the corpus."
+		choice = str(raw_input("\n  Which corpus should be used? > "))
+	return choice
+
+def generate(collection, corpus, xmls):
+	print ""
+	logger.status("Starting analysis.")
+	trainer = training.Trainer(collection, corpus, xmls)
+	trainer.run()
+	resultSet = trainer.results
+	logger.status("Finished anaylsis.")
+	logger.status("Starting generation.")
+
+if __name__ == '__main__':
+	startup()
