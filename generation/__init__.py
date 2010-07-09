e2e635a1ad645ebc87f04d46c31f2d8546f67e24
Johannes Emerich <johannes@emerich.de>
2010-07-09 02:30:40 +0200

    First working multi-tracking

--- generation/__init__.py
+++ generation/__init__.py
@@ -19,25 +19,28 @@ class Generator:
 	def __init__(self, resultSet):
 		""" Fills the generator with the available data. """
 		self._melody = resultSet['melody']
-		self._alphabet = self._melody.sampleSpace
+		self._instruments = resultSet['instruments']
+		self._bandSize = resultSet['bandSize']
+		self._activeInstruments = self._instruments.topN(self._bandSize)
+		self._alphabets = {}
+		for instrument in self._activeInstruments:
+			self._alphabets[instrument] = self._melody[instrument].sampleSpace
 	
-	def generate(self):
-		""" Generates a song from its available data on call. """
-		# Replace this with data from analysis
-		songlength = 50
+	def _generatePart(self, partName, measureNum, measureLen):
+		""" Generates a part from its available data on call. """
 		
 		# Choose a random history as starting point
-		history = random.choice(self._melody.conditions)
+		history = random.choice(self._melody[partName].conditions)
 		
 		logger.status("Starting from " + str(history))
 		
 		score = list(history)
 		
 		# fill the score with new notes
-		for i in range(songlength): 
+		for i in range(measureNum): 
 			
 			# selects random note c with Pr(c|history)
-			nextNote = self._predictNote(history)
+			nextNote = self._predictNote(history, partName)
 			
 			# add new note to list of generated notes
 			score.append(nextNote)
@@ -45,23 +48,39 @@ class Generator:
 			history = history[1:] + (nextNote,)
 			logger.status("Current history is " + str(history))
 		
-		# this way we can insert different parts as soon as we got more than one 
-		a = music21.stream.Part()
+		part = music21.stream.Part()
 		for note in score:
-			a.append(note)
+			part.append(note)
 		
+		return part
+	
+	def generate(self):
 		s = music21.stream.Score()
-		s.insert(0, a)
-		
+		for instrument in self._activeInstruments:
+			logger.status("Generating part for %s." % instrument)
+			# TODO determine measureNum and measureLen through analysis
+			s.insert(0, self._generatePart(instrument, 60, 4))
 		return s
 	
-	def _predictNote(self, realHistory):
+	def _completeMeasure(self, part, currQuarterLength, measureLength = 4):
+		""" Fills up one measure worth of rests. """
+		rest = music21.note.Rest()
+		if currQuarterLength < measureLength:
+			rest.quarterLength = measureLength - currQuarterLength
+			part.append(rest)
+			return part # TODO is this necessary? (used)
+		elif currQuarterLength == measureLength:
+			rest.quarterLength = measureLength
+			part.append(rest)
+			return part
+	
+	def _predictNote(self, realHistory, partName):
 		""" Predicts the next note from a history that is being provided. """
 		normalizedHistory = music42.normalizeNotes(realHistory)
 		
-		if self._melody.expected(normalizedHistory):
-			return music42.denormalizeNote(self._melody.expectedFuzz(normalizedHistory), realHistory)
+		if self._melody[partName].expected(normalizedHistory):
+			return music42.denormalizeNote(self._melody[partName].expectedFuzz(normalizedHistory), realHistory)
 		
 		# if nothing is found take a random note
 		logger.log("Failed to find prediction for history %s" % (normalizedHistory,))
-		return random.choice(self._alphabet)
+		return random.choice(self._alphabets[partName])
