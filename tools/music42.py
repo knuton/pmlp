95181d5cff7b8cde0a873a60fb7ecd3ab386f843
Johannes Emerich <johannes@emerich.de>
2010-08-03 00:33:44 +0200

    Add some tests

--- tools/music42.py
+++ tools/music42.py
@@ -6,7 +6,21 @@ cMinScale = [0,2,3,5,7,8,10]
 cFour = music21.note.Note()
 
 def normalizeNotes(noteSequence = []):
-	""" Normalizes a sequence of notes to one with same intervals, but starting on C4. """
+	""" Normalizes a sequence of notes to one with same intervals, but starting on C4.
+	
+	>>> import music21
+	>>> s = music21.stream.Stream()
+	>>> s.append(music21.note.Note('D'))
+	>>> s.append(music21.note.Note('G', type = 4))
+	>>> s.duration.quarterLength
+	5.0
+	>>> normalized = normalizeNotes(s)
+	>>> s2 = music21.stream.Stream()
+	>>> for note in normalized:
+	...   s2.append(note)
+	>>> s2.duration.quarterLength
+	5.0
+	"""
 	# We need to use an actual note with pitch to determine the distance to C4,
 	# not a rest or other note-like object.
 	firstNote = firstActualNote(noteSequence)
@@ -39,7 +53,19 @@ def makeCmaj(note):
 	return note
 
 def makeNote(noteObj):
-	""" Makes a standardized note from some note object. """
+	""" Makes a standardized note from some note object.
+	
+	Notes
+	>>> g4 = music21.note.Note('G', type = 4)
+	>>> g4Copy = makeNote(g4)
+	>>> g4.duration.quarterLength
+	4.0
+	
+	Chords
+	>>> gmaj4 = makeNote(music21.chord.Chord(['G', 'B', 'D'], type = 4))
+	>>> gmaj4.duration.quarterLength
+	4.0
+	"""
 	if noteObj.isNote:
 		note = music21.note.Note(type = noteObj.duration.quarterLength)
 		note.pitch.midi = noteObj.pitch.midi
@@ -52,3 +78,7 @@ def makeNote(noteObj):
 		return music21.chord.Chord(noteObj.pitchClasses, type = noteObj.duration.quarterLength)
 	else:
 		return music21.note.Note()
+
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
