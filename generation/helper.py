36e8d6cb98fd1eaeba361c292557eade0cd4172d
Johannes Emerich <johannes@emerich.de>
2010-09-03 11:02:21 +0200

    Fixed outro (I believe)

--- generation/helper.py
+++ generation/helper.py
@@ -1,13 +1,15 @@
 def combineSubparts(part, subparts = [], introOutro = [], numOfRepeats = 1):
 	""" Takes a part and a list of subparts and appends the subparts' contents to the part. """
 	# using repeatAppend because that creates a copy of the measure which helps keeping the right order
-	part.repeatAppend(introOutro, numOfRepeats)
+	for measure in introOutro:
+		part.repeatAppend(measure, numOfRepeats)
 	
 	for subpart in subparts:
 		for measure in subpart:
 			part.repeatAppend(measure, 1)
 	
-	part.repeatAppend(introOutro, numOfRepeats)
+	for measure in introOutro:
+		part.repeatAppend(measure, numOfRepeats)
 
 def resetContext(music21Obj):
 	""" Resets the context of a music21 object or a list of music21 objects. """
