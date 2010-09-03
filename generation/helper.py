1b91c40dfeebc46fee8d0518bad71fc82e3e7e93
Johannes Emerich <johannes@emerich.de>
2010-09-03 14:20:48 +0200

    Fix this time for real

--- generation/helper.py
+++ generation/helper.py
@@ -1,15 +1,23 @@
+import music21.stream
+
 def combineSubparts(part, subparts = [], introOutro = [], numOfRepeats = 1):
 	""" Takes a part and a list of subparts and appends the subparts' contents to the part. """
 	# using repeatAppend because that creates a copy of the measure which helps keeping the right order
-	for measure in introOutro:
-		part.repeatAppend(measure, numOfRepeats)
+	if isinstance(introOutro, music21.stream.Measure):
+		part.repeatAppend(introOutro, numOfRepeats)
+	else:
+		for measure in introOutro:
+			part.repeatAppend(measure, numOfRepeats)
 	
 	for subpart in subparts:
 		for measure in subpart:
 			part.repeatAppend(measure, 1)
 	
-	for measure in introOutro:
-		part.repeatAppend(measure, numOfRepeats)
+	if isinstance(introOutro, music21.stream.Measure):
+		part.repeatAppend(introOutro, numOfRepeats)
+	else:
+		for measure in introOutro:
+			part.repeatAppend(measure, numOfRepeats)
 
 def resetContext(music21Obj):
 	""" Resets the context of a music21 object or a list of music21 objects. """
